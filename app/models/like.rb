7fcc71d9f0a03c7da9fbed7fba94c1a15385ab74
shizimi50 <mjrwrbbc@gmail.com>
2022-01-06 23:52:32 +0900

    add: like機能新規追加

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,13 @@
 class Like < ApplicationRecord
     # =====Validation======
     validates :favorite_data_id, :user_id, presence: true #空の値に対してバリデーション設定
+    validates :user_id, uniqueness: { scope: :favorite_data_id } # user_id と favorite_data_idの組み合わせを一意性に
     # ＝＝＝＝＝＝＝＝＝＝＝＝＝
 
     # ＝＝＝＝関連付け＝＝＝＝＝
     belongs_to :favorite_data
     belongs_to :user
-    has_many :bookmarks
+    # has_many :bookmarks Validation failed: Like must exist対策のため一旦コメントアウト
     # ＝＝＝＝＝＝＝＝＝＝＝＝＝
 
     # ＝＝フレームワーク関連＝＝
